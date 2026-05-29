33d6d02b53c34e413316f8a62b0113d8fb9aea4a
Abel1518 <abelrohmad1518@gmail.com>
2026-05-29 23:32:00 +0700

    Add files via upload

---
+++ api/soal4(2025).php
@@ -0,0 +1,35 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include 'koneksi.php';
+
+$data = array();
+
+$sql = "
+SELECT
+    peminjaman.nama_peminjam,
+    buku.judul_buku,
+    buku.penulis,
+    peminjaman.tanggal_pinjam,
+    buku.tahun_terbit
+FROM peminjaman
+JOIN buku
+ON peminjaman.buku_id = buku.id
+WHERE buku.tahun_terbit = 2025
+";
+
+$query = mysqli_query($conn, $sql);
+
+while($row = mysqli_fetch_assoc($query)) {
+
+    $data[] = $row;
+
+}
+
+echo json_encode($data, JSON_PRETTY_PRINT);
+
+mysqli_close($conn);
+
+?>
